200fc78b126b3f08326cec5c11c12e713059d31f
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-06-11 16:44:08 -0400

    add insert solution for binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,25 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    new_branch = BinarySearchTree(value)
+    current_branch = self
+
+    # seek this value's spot
+    # traverse the tree until we find the missing
+    # branch that can hold this value
+    while True:
+      if current_branch.value <= value:
+        if current_branch.right:
+          current_branch = current_branch.right
+        else:
+          current_branch.right = new_branch
+          break
+      elif current_branch.value > value:
+        if current_branch.left:
+          current_branch = current_branch.left
+        else:
+          current_branch.left = new_branch
+          break
 
   def contains(self, target):
     pass
