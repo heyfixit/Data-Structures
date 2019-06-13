86725ee9b518205d41ef301ca16d6b9af5899e11
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-06-13 16:49:30 -0400

    copy max_heap to generic_heap

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,75 @@
 class Heap:
-  def __init__(self, comparator):
+  def __init__(self, comparator = None):
     self.storage = []
-    self.comparator = comparator
+    if comparator == None:
+      self.comparator = lambda a,b: a > b
+
 
   def insert(self, value):
-    pass
+    # add value to next available spot
+    # swap up the heap until
+    # the 'heap property' is valid
+    # wikipedia says any value at index n might have
+    # children at index 2n + 1 and 2n + 2
+
+    self.storage.append(value)
+    current_index = len(self.storage) - 1
+
+    self._bubble_up(current_index)
 
   def delete(self):
-    pass
+    # return None if the Heap is empty
+    if len(self.storage) == 0:
+      return None
 
-  def get_priority(self):
-    pass
+    deleted_item = self.storage.pop(0)
+
+    # do nothing if that was the last item
+    if len(self.storage) == 0:
+      return deleted_item
+
+    # shuffle the last item to the front
+    self.storage.insert(0, self.storage.pop(-1))
+
+    self._sift_down(0)
+
+    return deleted_item
+
+  def get_max(self):
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    parent_index = (index - 1) // 2
+    if(parent_index >= 0
+    and self.comparator(self.storage[index], self.storage[parent_index])):
+    # and self.storage[parent_index] < self.storage[index]):
+      self.storage[index], self.storage[parent_index] = self.storage[parent_index], self.storage[index]
+      self._bubble_up(parent_index)
 
   def _sift_down(self, index):
-    pass
+    left_index = 2 * index + 1
+    right_index = 2 * index + 2
+
+    # always swap with the smallest child
+    if(right_index < self.get_size()):
+      # then we have 2 children
+      # if self.storage[left_index] >= self.storage[right_index]:
+      if self.comparator(self.storage[left_index], self.storage[right_index]):
+        # if self.storage[left_index] > self.storage[index]:
+        self._bubble_up(left_index)
+        self._sift_down(left_index)
+      # elif self.storage[right_index] > self.storage[left_index]:
+      elif self.comparator(self.storage[right_index], self.storage[left_index]):
+        # if self.storage[right_index] > self.storage[index]:
+        self._bubble_up(right_index)
+        self._sift_down(right_index)
+    elif(left_index < self.get_size()):
+      # we have one child
+      # if self.storage[left_index] > self.storage[index]:
+      if self.comparator(self.storage[left_index], self.storage[right_index]):
+        self._bubble_up(left_index)
+        self._sift_down(left_index)
+
